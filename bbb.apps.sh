00d8682b646c9ce6f662d278d0f862a683c5d35d
Alexander Merkulov <sasha@merqlove.ru>
2014-11-24 18:21:06 +0300

    v0.9.0-beta Custom Init

---
+++ bbb.apps.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+set -x
+
+RED5_DIR=/usr/share/red5
+BBB_DIR=$(pwd)
+
+sudo chmod -R 777 $RED5_DIR/webapps/
+
+cd $BBB_DIR
+
+VOICE=$BBB_DIR/bbb-voice
+VIDEO=$BBB_DIR/bbb-video
+APPS=$BBB_DIR/bigbluebutton-apps
+
+echo "Building apps"
+cd $APPS
+gradle resolveDeps
+gradle clean war deploy
+
+echo "Building voice"
+cd $VOICE
+gradle resolveDeps
+gradle clean war deploy
+
+echo "Building video"
+cd $VIDEO
+gradle resolveDeps
+gradle clean war deploy
+
+cd $BBB_DIR
+
+sudo chown -R red5.adm $RED5_DIR
